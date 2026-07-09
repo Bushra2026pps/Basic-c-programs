75a953fbc008fb2bc0be6a9078acc3d7837ea85e
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 21:19:02 +0530

    Add files via upload

---
+++ grades using switch case.c.cpp
@@ -0,0 +1,42 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include <stdio.h>
+
+int main() {
+    char grade;
+
+    printf("Enter the student's grade (A, B, C, D, F): ");
+    scanf("%c", &grade);
+
+    switch(grade) {
+        case 'A':
+        case 'a':
+            printf("Excellent");
+            break;
+
+        case 'B':
+        case 'b':
+            printf("Very Good");
+            break;
+
+        case 'C':
+        case 'c':
+            printf("Good");
+            break;
+
+        case 'D':
+        case 'd':
+            printf("Pass");
+            break;
+
+        case 'F':
+        case 'f':
+            printf("Fail");
+            break;
+
+        default:
+            printf("Invalid Grade");
+    }
+
+    return 0;
+}
