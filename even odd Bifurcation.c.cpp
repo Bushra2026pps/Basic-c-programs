0d2ed0c2b6e326df64c95e26e6e717fe0c82c50f
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 21:21:56 +0530

    Add files via upload

---
+++ even odd Bifurcation.c.cpp
@@ -0,0 +1,31 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include <stdio.h>
+
+int main() {
+    int a[10], i, n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Even numbers are:\n");
+    for(i = 0; i < n; i++) {
+        if(a[i] % 2 == 0) {
+            printf("%d ", a[i]);
+        }
+    }
+
+    printf("\nOdd numbers are:\n");
+    for(i = 0; i < n; i++) {
+        if(a[i] % 2 != 0) {
+            printf("%d ", a[i]);
+        }
+    }
+
+    return 0;
+}
