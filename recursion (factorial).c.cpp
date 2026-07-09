61066f3ff11f6914715c6224011694f01879a791
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 20:58:25 +0530

    Add files via upload

---
+++ recursion (factorial).c.cpp
@@ -0,0 +1,25 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num, fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    fact = factorial(num);
+
+    printf("Factorial of %d = %d", num, fact);
+
+    return 0;
+}
