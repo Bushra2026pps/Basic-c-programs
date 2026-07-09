6e8eb928f8fef12c39c3d5e1a12e97ba07700cfa
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 21:06:04 +0530

    Add files via upload

---
+++ ATM pin.c.cpp
@@ -0,0 +1,22 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include <stdio.h>
+
+int main()
+{
+    int pin;
+
+    printf("Enter ATM PIN: ");
+    scanf("%d", &pin);
+
+    if (pin == 1234)
+    {
+        printf("PIN is correct. Access Granted.");
+    }
+    else
+    {
+        printf("Incorrect PIN. Access Denied.");
+    }
+
+    return 0;
+}
