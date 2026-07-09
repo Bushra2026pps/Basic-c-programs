28094e1ad785d2ec1ec5450b1d298df2d4bbf187
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 21:19:20 +0530

    Add files via upload

---
+++ even odd using if else.cpp
@@ -0,0 +1,19 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is an Even number.", num);
+    }
+    else {
+        printf("%d is an Odd number.", num);
+    }
+
+    return 0;
+}
