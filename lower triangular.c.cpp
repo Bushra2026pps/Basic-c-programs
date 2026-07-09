74e09efc6ade91e7c77dedd85684389552ef82b1
Bushra2026pps <bushradeshmukh769@gmail.com>
2026-07-09 20:36:27 +0530

    Add files via upload

---
+++ lower triangular.c.cpp
@@ -0,0 +1,15 @@
+// Bushra Junaid Deshmukh 
+// 2503033111372028
+#include<stdio.h>
+int main()
+{
+int i,j;
+for(i=1;i<=3;i++){
+for(j=1;j<=i;j++){
+printf("* ");
+}
+printf("\n");
+}
+return 0;
+}
+
